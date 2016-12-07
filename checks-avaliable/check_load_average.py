707431b1d155b75b51f51361fd93205ece1f8caa
pastor <ara.sadoyan@netangels.net>
2016-12-07 16:25:57 +0400

    minor fixes

--- checks-avaliable/check_load_average.py
+++ checks-avaliable/check_load_average.py
@@ -9,10 +9,13 @@ cluster_name = config.get('SelfConfig', 'cluster_name')
 
 
 def run_load_average():
-    try:
-        proc_loadavg=open('/proc/loadavg', "r")
-    except IOError as e:
-        print "I/O error({0}): {1}".format(e.errno, e.strerror)
+
+    cpucount = 0
+    for line in open("/proc/stat", "r").xreadlines():
+        if 'cpu' in line:
+            cpucount += 1
+    cpucount -=1
+    load_start_worry_about = format(float(cpucount)/3, '.3g')
 
     hostname = socket.getfqdn()
     check_type = 'system'
@@ -20,20 +23,23 @@ def run_load_average():
     push = __import__('pushdata')
     jsondata=push.JonSon()
     jsondata.create_data()
-
+    timestamp = int(datetime.datetime.now().strftime("%s"))
     try:
-        for line in proc_loadavg:
-            timestamp = int(datetime.datetime.now().strftime("%s"))
-            columns = line.split()
-            for loadavg in range(0, 3):
-                if loadavg is 0:
-                    jsondata.gen_data('sys_load_1',timestamp, columns[loadavg], hostname, check_type, cluster_name)
-                elif loadavg is 1:
-                    jsondata.gen_data('sys_load_5',timestamp, columns[loadavg], hostname, check_type, cluster_name)
-                elif loadavg is 2:
-                    jsondata.gen_data('sys_load_15',timestamp, columns[loadavg], hostname, check_type, cluster_name)
+
+        proc_loadavg=open("/proc/loadavg", "r").readline().split()
+
+        if float(proc_loadavg[0]) < load_start_worry_about:
+            alert_level = -3
+        else:
+            alert_level = 0
+
+        jsondata.gen_data('sys_load_1', timestamp, proc_loadavg[0], hostname, check_type, cluster_name, alert_level)
+        jsondata.gen_data('sys_load_5', timestamp, proc_loadavg[1], hostname, check_type, cluster_name, alert_level)
+        jsondata.gen_data('sys_load_15', timestamp, proc_loadavg[2], hostname, check_type, cluster_name, alert_level)
+
         jsondata.put_json()
         jsondata.truncate_data()
+
     except Exception as e:
         push = __import__('pushdata')
         push.print_error(__name__ , (e))
