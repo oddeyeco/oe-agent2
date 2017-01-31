fbd3cecc6a2f22a80cbb52da43636434a7a091c6
pastor <ara.sadoyan@netangels.net>
2017-01-31 17:37:10 +0400

    HBase Region G1 Last GC

--- checks-avaliable/check_hbase_regionserver.py
+++ checks-avaliable/check_hbase_regionserver.py
@@ -43,6 +43,23 @@ def run_hbase_regionserver():
                         parnew_value=stats_keys[stats_x]['LastGcInfo']['duration']
                         mon_values.update({parnew_key: parnew_value})
 
+        for stats_x in range(0, len(stats_keys)):
+            for k, v in enumerate(('java.lang:type=GarbageCollector,name=G1 Young Generation', 'java.lang:type=GarbageCollector,name=G1 Old Generation')):
+                if v in stats_keys[stats_x]['name']:
+                    if k is 0:
+                        g1_young_key='hregion_heap_G1_Young_LastGcInfo'
+                        g1_young_value=stats_keys[stats_x]['LastGcInfo']['duration']
+                        mon_values.update({g1_young_key: g1_young_value})
+                    if k is 1:
+                        if stats_keys[stats_x]['LastGcInfo'] is not None:
+                            g1_old_key='hregion_heap_G1_Old_LastGcInfo'
+                            g1_old_value=stats_keys[stats_x]['LastGcInfo']['duration']
+                            mon_values.update({g1_old_key: g1_old_value})
+                        else:
+                            g1_old_key='hregion_heap_G1_Old_LastGcInfo'
+                            g1_old_value=0
+                            mon_values.update({g1_old_key: g1_old_value})
+
         for stats_index in range(0, len(stats_keys)):
             for values in node_rated_keys:
                 if values in stats_keys[stats_index]:
