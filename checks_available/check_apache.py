ed531c5f3e5308215df2a53d77a559dc1bbd84f5
Ara Sadoyan <ara.sadoyan@netangels.net>
2017-06-12 14:04:03 +0400

    Check Apache fix

--- checks_available/check_apache.py
+++ checks_available/check_apache.py
@@ -16,9 +16,9 @@ cluster_name = lib.getconfig.getparam('SelfConfig', 'cluster_name')
 def runcheck():
     try:
         if curl_auth is True:
-            lib.commonclient.httpget(__name__, apache_url, apache_auth)
+            data = lib.commonclient.httpget(__name__, apache_url, apache_auth)
         else:
-            lib.commonclient.httpget(__name__, apache_url)
+            data = lib.commonclient.httpget(__name__, apache_url)
 
         jsondata = lib.pushdata.JonSon()
         jsondata.prepare_data()
@@ -27,20 +27,20 @@ def runcheck():
         metrics_rated = ('Total Accesses', 'Total kBytes')
         metrics_stuck = ('ReqPerSec', 'BytesPerSec', 'BytesPerReq', 'BusyWorkers', 'IdleWorkers')
         timestamp = int(datetime.datetime.now().strftime("%s"))
-        for line in t.contents.split('\n'):
+        for line in data.split('\n'):
             for searchitem in  metrics_stuck:
                 if searchitem in line:
                     key = line.split(' ')[0].replace(':', '')
                     value = line.split(' ')[1]
-                    jsondata.gen_data('apache_'+key, timestamp, value, lib.pushdata.hostname, check_type, cluster_name)
-            for searchitem in  metrics_rated:
+                    jsondata.gen_data('apache_'+key.lower(), timestamp, value, lib.pushdata.hostname, check_type, cluster_name)
+            for searchitem in metrics_rated:
                 reaction = 0
                 metr_type = 'Rate'
                 if searchitem in line:
                     key = line.split(' ')[0]+line.split(' ')[1].replace(':', '')
                     value = line.split(' ')[2]
                     value_rate = rate.record_value_rate(key, value, timestamp)
-                    jsondata.gen_data('apache_'+key, timestamp, value_rate, lib.pushdata.hostname, check_type, cluster_name, reaction, metr_type)
+                    jsondata.gen_data('apache_'+key.lower(), timestamp, value_rate, lib.pushdata.hostname, check_type, cluster_name, reaction, metr_type)
 
         jsondata.put_json()
     except Exception as e:
